037de861af1a1ba38fd68804ea1ffd065822be70
Ralica Yordanova <ralica.yordano@gmail.com>
2026-08-10 00:02:56 +0200

    Update test_jewelry_item.py

--- tests/unit/test_jewelry_item.py
+++ tests/unit/test_jewelry_item.py
@@ -1,1 +1,14 @@
+from src.models.jewelry_item import JewelryItem
 
+
+def test_create_valid_jewelry_item():
+    item = JewelryItem(
+        brand="Miriam Haskell",
+        category="Bracelet",
+        condition="Very Good",
+        purchase_price=70.0
+    )
+
+    assert item.brand == "Miriam Haskell"
+    assert item.category == "Bracelet"
+    assert item.purchase_price == 70.0
